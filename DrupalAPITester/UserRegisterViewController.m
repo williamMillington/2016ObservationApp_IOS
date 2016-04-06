d01bac5ac1b720c9edf5d3d4dfc702008fe25d10
joncudmore <jon.cudmore2@gmail.com>
2016-04-06 16:52:32 -0300

    Fixed error
    Had to move the UserProfileView files back

--- DrupalAPITester/UserRegisterViewController.m
+++ DrupalAPITester/UserRegisterViewController.m
@@ -64,12 +64,12 @@
 
 #pragma mark - Navigation
 /*
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 // Return from terms and conditions view to registration view
 -(IBAction)returnFromTerms:(id)sender {
