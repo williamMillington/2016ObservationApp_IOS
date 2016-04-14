62dc4bf09f35601a877e767d682b8c42dca983d9
joncudmore <jon.cudmore2@gmail.com>
2016-04-14 18:31:02 -0300

    Apply license
    Apply license to test

--- DrupalAPITesterTests/DrupalAPITesterTests.m
+++ DrupalAPITesterTests/DrupalAPITesterTests.m
@@ -2,9 +2,23 @@
 //  DrupalAPITesterTests.m
 //  DrupalAPITesterTests
 //
-//  Created by Jonathan Cudmore on 2016-04-08.
-//  Copyright © 2016 William Millington. All rights reserved.
-//
+/**	 ObservationApp, Copyright 2016, University of Prince Edward Island,
+ *    550 University Avenue, C1A4P3,
+ *    Charlottetown, PE, Canada
+ *
+ * 	 @author Jonathan Cudmore<jrcudmore@upei.ca>
+ *
+ *   This file is part of ObservationApp.
+ *
+ *   ObservationApp is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Observation App.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import <XCTest/XCTest.h>
 #import "LoginViewController.h"
