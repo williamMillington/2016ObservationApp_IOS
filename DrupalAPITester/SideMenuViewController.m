e87db462820184089ee2c24bdae43f8ca1409897
joncudmore <jon.cudmore2@gmail.com>
2016-04-14 18:29:32 -0300

    Apply License
    Added license blurb to beginning of every file

--- DrupalAPITester/SideMenuViewController.m
+++ DrupalAPITester/SideMenuViewController.m
@@ -2,10 +2,23 @@
 //  SideMenuViewController.m
 //  DrupalAPITester
 //
-//  Created by William Millington on 2016-03-09.
-//  Copyright © 2016 William Millington. All rights reserved.
-//
-
+/**	 ObservationApp, Copyright 2016, University of Prince Edward Island,
+ *    550 University Avenue, C1A4P3,
+ *    Charlottetown, PE, Canada
+ *
+ * 	 @author William Millington<wmillington@upei.ca>
+ *
+ *   This file is part of ObservationApp.
+ *
+ *   ObservationApp is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Observation App.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #import "SideMenuViewController.h"
 
 @interface SideMenuViewController ()
