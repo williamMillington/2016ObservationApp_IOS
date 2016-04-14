8463982a47736c07e3c1acde5c0f7e065f28cd68
joncudmore <jon.cudmore2@gmail.com>
2016-04-14 18:21:44 -0300

    Add interface inactions and outlets
    Included the interface builder elements to the header files

--- UserViewController.h
+++ UserViewController.h
@@ -11,4 +11,24 @@
 
 @interface UserViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIImageView *profilePicture;
+@property (weak, nonatomic) IBOutlet UILabel *userName;
+@property (weak, nonatomic) IBOutlet UILabel *emailAddress;
+@property (weak, nonatomic) IBOutlet UILabel *location1;
+@property (weak, nonatomic) IBOutlet UILabel *location2;
+
+@property (weak, nonatomic) IBOutlet UILabel *countryL1;
+@property (weak, nonatomic) IBOutlet UILabel *firstAddressL1;
+@property (weak, nonatomic) IBOutlet UILabel *secondAddressL1;
+@property (weak, nonatomic) IBOutlet UILabel *provinceL1;
+@property (weak, nonatomic) IBOutlet UILabel *postalCodeL1;
+@property (weak, nonatomic) IBOutlet UILabel *cityL1;
+
+@property (weak, nonatomic) IBOutlet UILabel *countryL2;
+@property (weak, nonatomic) IBOutlet UILabel *firstAddressL2;
+@property (weak, nonatomic) IBOutlet UILabel *secondAddressL2;
+@property (weak, nonatomic) IBOutlet UILabel *provinceL2;
+@property (weak, nonatomic) IBOutlet UILabel *postalCodeL2;
+@property (weak, nonatomic) IBOutlet UILabel *cityL2;
+
 @end
--- UserViewController.m
+++ UserViewController.m
@@ -13,26 +13,6 @@
 
 @interface UserViewController ()
 
-@property (weak, nonatomic) IBOutlet UIImageView *profilePicture;
-@property (weak, nonatomic) IBOutlet UILabel *userName;
-@property (weak, nonatomic) IBOutlet UILabel *emailAddress;
-@property (weak, nonatomic) IBOutlet UILabel *location1;
-@property (weak, nonatomic) IBOutlet UILabel *location2;
-
-@property (weak, nonatomic) IBOutlet UILabel *countryL1;
-@property (weak, nonatomic) IBOutlet UILabel *firstAddressL1;
-@property (weak, nonatomic) IBOutlet UILabel *secondAddressL1;
-@property (weak, nonatomic) IBOutlet UILabel *provinceL1;
-@property (weak, nonatomic) IBOutlet UILabel *postalCodeL1;
-@property (weak, nonatomic) IBOutlet UILabel *cityL1;
-
-@property (weak, nonatomic) IBOutlet UILabel *countryL2;
-@property (weak, nonatomic) IBOutlet UILabel *firstAddressL2;
-@property (weak, nonatomic) IBOutlet UILabel *secondAddressL2;
-@property (weak, nonatomic) IBOutlet UILabel *provinceL2;
-@property (weak, nonatomic) IBOutlet UILabel *postalCodeL2;
-@property (weak, nonatomic) IBOutlet UILabel *cityL2;
-
 @end
 
 @implementation UserViewController
