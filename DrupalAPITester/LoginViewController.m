e825fc984cb0afa49baeb31baaabae9c07d7968a
William Millington <willington1974@hotmail.com>
2016-04-15 12:49:00 -0300

    Added GPL

--- DrupalAPITester/LoginViewController.m
+++ DrupalAPITester/LoginViewController.m
@@ -5,6 +5,22 @@
 //  Created by William Millington on 2016-03-05.
 //  Modifications by Jonathan Cudmore
 //
+//
+// This file is part of DrupalAPITester.
+//
+// DrupalAPITester is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Foobar is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+// ---------------------------------------------------------------------------------
 
 #import "LoginViewController.h"
 #import "UserViewController.h"
--- DrupalAPITester/UserRegisterViewController.m
+++ DrupalAPITester/UserRegisterViewController.m
@@ -5,6 +5,22 @@
 //  Created by Jonathan Cudmore on 2016-04-04.
 //  Copyright © 2016 William Millington. All rights reserved.
 //
+// This file is part of DrupalAPITester.
+//
+// DrupalAPITester is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Foobar is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+// ---------------------------------------------------------------------------------
+//
 
 #import "UserRegisterViewController.h"
 #import "SWRevealViewController.h"
